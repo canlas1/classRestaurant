17dd06cd98e5fded3375161174fbdb9373b50517
lasperilla <l.asperilla@gmail.com>
2017-05-06 12:16:00 -0400

    edited server

--- server.js
+++ server.js
@@ -1,13 +1,27 @@
 var bodyParser = require('body-parser')
 var express = require('express')
 var app = express()
+var path = require("path");
+var PORT = 3000;
 
  
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
+// app.get('/', function (req, res) {
+//   res.sendFile('Hello World')
+// })
+
+app.get("/", function(req, res) {
+ res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get("/reserve", function(req, res) {
+ res.sendFile(path.join(__dirname, "reservation.html"));
+});
+
+app.get("/tables", function(req, res) {
+ res.sendFile(path.join(__dirname, "tables.html"));
+});
  
-app.listen(3000);
+// app.listen(3000);
 
 // parse application/x-www-form-urlencoded 
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -21,4 +35,6 @@ app.use(function (req, res) {
   res.end(JSON.stringify(req.body, null, 2))
 })
 
-
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
