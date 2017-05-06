5563925780dbecb7f0a40868d01e0cbccceda189
lasperilla <l.asperilla@gmail.com>
2017-05-06 13:55:54 -0400

    edited tables, reservation, server

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var express = require('express')
 var app = express()
 var path = require("path");
 var PORT = 3000;
+var reservationsArr =[{reserve_name:"law", reserve_phone:"123", reserve_email:"law@law.com", reserve_uniqueID:"123"}];
 
  
 // app.get('/', function (req, res) {
@@ -19,6 +20,22 @@ app.get("/reserve", function(req, res) {
 
 app.get("/tables", function(req, res) {
  res.sendFile(path.join(__dirname, "tables.html"));
+});
+
+app.get("/api/tables", function(req, res) {
+	res.json(reservationsArr)
+});
+
+app.post("/api/new", function(req, res) {
+  var newReservation = req.body;
+  newReservation.routeName = newReservation.name.replace(/\s+/g, "").toLowerCase();
+
+  console.log(newReservation);
+
+  reservationsArr.push(newReservation);
+
+  res.json(newReservation);
+  console.log(reservationsArr)
 });
  
 // app.listen(3000);
